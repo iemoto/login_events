b61f02d5f6360d1055f947fc52c647932ccd4905
iemoto <57856680+iemoto@users.noreply.github.com>
2020-01-19 14:04:26 +0900

    user on events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+  
+  def create
+    @event = Event.create(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new 
+    end
+  end
+
+  private
+  def event_params
+      params.require(:event).permit(:title, :description, :start_date, :end_date).merge(user_id: current_user.id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
-  def index
 
+  def index
+    @events = current_user.events
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email)
+  end
+
+
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
